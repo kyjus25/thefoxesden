9964a85081a9b203cd5167584d2827633d4f2b29
Justin White <kyjus25@gmail.com>
2020-10-28 16:18:58 -0500

    Updates

---
+++ enjin-1.0.1/sub-page/sub-top.js
@@ -0,0 +1,6 @@
+document.body.className += ' is-preload';
+
+$( document ).ready(function() {
+    $( "#menu" ).append( $( "#enjin-bar .right" ) );
+    $( "#menu" ).append( $( "#enjin-bar-likes" ) );
+});
